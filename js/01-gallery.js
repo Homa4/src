29d26c3a9f3fa050efd292a171d9e905aae39307
Timur Naboka <tn3996580@gmail.com>
2023-06-10 23:34:53 +0300

    do task 1,2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+// Створити елементи списку gallery items 
+fillGalleryList();
 
 console.log(galleryItems);
+
+gallery.addEventListener('click', handleClick)
+
+
+function fillGalleryList() {
+    for (const item of galleryItems) {
+        gallery.innerHTML += `<li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+            </a>
+        </li>`
+    }
+}
+
+
+function handleClick(event) {
+    event.preventDefault();
+    if (event.target === gallery) {
+        return;
+    }
+    const link = event.target.href || event.target.dataset.source;
+    basicLightbox.create(`<img src="${link}" width="800" height="600">`).show();
+
+}
