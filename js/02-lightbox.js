aa24dc15500bfdc2fd8b72afe7c2b2fb2e1cbcb2
Timur Naboka <tn3996580@gmail.com>
2023-06-10 23:34:24 +0300

    do task 1,2 exept final add text

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+fillGalleryList();
 
 console.log(galleryItems);
+
+gallery.addEventListener('click', handleClick)
+
+function fillGalleryList() {
+    for (const item of galleryItems) {
+        gallery.innerHTML += `
+            <li class="gallery__item">
+                <a class="gallery__link" href="${item.original}">
+                    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                </a>
+            </li>
+        `
+    }
+}
+
+function handleClick(event) {
+    event.preventDefault();
+    if (event.target === gallery) {
+        return;
+    }
+    let lightbox = new SimpleLightbox('.gallery a',{captions:true,});
+    lightbox.on('show.simplelightbox', function () {
+        // do something…
+        lightbox.captionPosition
+    });  
+}
+
